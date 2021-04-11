7d671503a2d1d79fb4614973883bf0599e4ce681
Ancientear <1136395374@qq.com>
2021-04-11 21:08:34 +0800

    点击cell触发的方法

--- SimpleApp/ViewController.m
+++ SimpleApp/ViewController.m
@@ -39,7 +39,7 @@
 
 @end
 
-@interface ViewController() <UITableViewDataSource>
+@interface ViewController() <UITableViewDataSource,UITableViewDelegate>
 
 @end
 
@@ -51,7 +51,21 @@
     
     UITableView *tableView = [[UITableView alloc] initWithFrame:self.view.bounds];
     tableView.dataSource = self;
+    //触发方法
+    tableView.delegate = self;
     [self.view addSubview:tableView];
+}
+
+//高度
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath{
+    return 100;
+}
+//点击触发的方法
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath{
+    UIViewController *controller = [[UIViewController alloc]init];
+    controller.title = [NSString stringWithFormat:@"%@" , @(indexPath.row)];
+    [self.navigationController pushViewController:controller animated:YES];
+    
 }
 
 
@@ -67,10 +81,18 @@
     if(!cell){
         cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleSubtitle reuseIdentifier:@"id"];
     }
-    cell.textLabel.text = @"主标题";
+    //cell.textLabel.text = @"主标题";
+    cell.textLabel.text = [NSString stringWithFormat:@"主标题 - %@",@(indexPath.row)];
     cell.detailTextLabel.text = @"副标题";
     cell.imageView.image = [UIImage imageNamed:@"/Users/shenfan/OCProjects/SimpleApp/SimpleApp/Image/right_disable.png"];
     return cell;
 }
 
+
+
+/*
+ 1、创建UITableView，设置delegate和datasource，通过两个delegate
+ 2、选择实现UITableViewDataSource中方法，行数、cell复用
+ 3、选择实现UITableViewDelegate中方法（高度、headerFooter、点击）
+ */
 @end
