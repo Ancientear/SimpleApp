8785fb351d2d18b4abf762aaefc12bce743c78f4
Ancientear <1136395374@qq.com>
2021-04-11 10:00:04 +0800

    测试UIView的生命周期

--- SimpleApp/ViewController.m
+++ SimpleApp/ViewController.m
@@ -7,13 +7,46 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+
+
+
+@interface TestView : UIView
+@end
+
+
+@implementation  TestView
+
+
+- (instancetype)init{
+    self = [super init];
+    if(self){
+    }
+    return self;
+}
+
+//测试UIView的生命周期
+- (void)willMoveToSuperview:(nullable UIView *)newSuperview{
+    [super willMoveToSuperview:newSuperview];
+    
+}
+- (void)didMoveToSuperview{
+    [super didMoveToSuperview];
+    
+}
+- (void)willMoveToWindow:(nullable UIWindow *)newWindow{
+    [super willMoveToWindow :newWindow];
+}
+- (void)didMoveToWindow{
+    [super didMoveToWindow];
+}
 
 @end
+
 
 @implementation ViewController
 
 - (void)viewDidLoad {
+    self.view.backgroundColor = [UIColor whiteColor];
     [super viewDidLoad];
     // Do any additional setup after loading the view.
     [self.view addSubview:({
@@ -23,7 +56,13 @@
         label.center = CGPointMake(self.view.frame.size.width / 2, self.view.frame.size.height /2);
         label;
     })];
+    
+    
+    TestView * view = [[TestView alloc]init];
+    view.backgroundColor = [UIColor redColor];
+    view.frame = CGRectMake(100,100,100,100);
+    [self.view addSubview:view];
+    
 }
-
 
 @end
