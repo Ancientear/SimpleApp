9b2acb436bfd02ef1c04899eebbbd22c54d03c2f
Ancientear <1136395374@qq.com>
2021-04-14 17:23:43 +0800

    加载列表先从本地取数据，再网络拉数据更新本地数据

--- SimpleApp/News/Models/GTListLoader.m
+++ SimpleApp/News/Models/GTListLoader.m
@@ -12,12 +12,17 @@
 
 - (void)GTListLoaderFinishBlock:(GTListLoaderFinishBlock)finishBlock {
 
+    
+    //在请求的头部需要读取一下
+    NSArray<GTListItem *> *listdata = [self _readDataFromLocal];
+    if(listdata){
+        finishBlock(YES,listdata);
+    }
+    
 	NSString *urlString = @"http://v.juhe.cn/toutiao/index?type=top&key=97ad001bfcc2082e2eeaf798bad3d54e";
 	NSURL *listURL = [NSURL URLWithString:urlString];
-    
 	NSURLSession *session = [NSURLSession sharedSession];
-    
-    //为了循环引用对self进行处理
+	//为了循环引用对self进行处理
 	__weak typeof(self) weakSelf = self;
 	NSURLSessionDataTask *dataTask = [session dataTaskWithURL:listURL completionHandler:^(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error) {
 	                                          __strong typeof (weakSelf) strongSelf = weakSelf;
@@ -49,6 +54,27 @@
 
 }
 
+#pragma mark - private method
+//网络获取速度太慢，可以考虑先从本地取数据，先占位。当列表有新的数据拉取回来之后再重新替换
+//实现一个简单的读取逻辑
+- (NSArray<GTListItem *> *)_readDataFromLocal {
+	NSArray *pathArray = NSSearchPathForDirectoriesInDomains(NSCachesDirectory, NSUserDomainMask, YES);
+	NSString *cachePath = [pathArray firstObject];
+    //创建路径
+	NSString *listDataPath = [cachePath stringByAppendingPathComponent:@"GTData/list"];
+    //使用fileManager来读取数据
+	NSFileManager *fileManager = [NSFileManager defaultManager];
+	NSData *readListData = [fileManager contentsAtPath:listDataPath];
+
+    //将数据反序列化
+	id unarchiveObj = [NSKeyedUnarchiver unarchivedObjectOfClasses:[NSSet setWithObjects:[NSArray class], [GTListItem class],nil] fromData:readListData error:nil];
+    //判断数据是否有效
+    if ([unarchiveObj isKindOfClass:[NSArray class]] && [unarchiveObj count] > 0) {
+        return (NSArray<GTListItem *> *)unarchiveObj;
+    }
+    return nil;
+}
+
 - (void)_archiveListDataWithArray:(NSArray<GTListItem *> *)array {
 	NSArray *pathArray = NSSearchPathForDirectoriesInDomains(NSCachesDirectory, NSUserDomainMask, YES);
 	NSString *cachePath = [pathArray firstObject];
@@ -65,18 +91,22 @@
 	//NSData *listData = [@"abc" dataUsingEncoding:NSUTF8StringEncoding];
 
 	//序列化list
-    NSData *listData =  [NSKeyedArchiver archivedDataWithRootObject:array requiringSecureCoding:YES error:nil];
+	NSData *listData =  [NSKeyedArchiver archivedDataWithRootObject:array requiringSecureCoding:YES error:nil];
 	[fileManager createFileAtPath:listDataPath contents:listData attributes:nil];
-    
-    //读取相关文件
-    NSData *readListData = [fileManager contentsAtPath:listDataPath];
+
+	//
+
+
+	//读取相关文件
+//    NSData *readListData = [fileManager contentsAtPath:listDataPath];
+
 //    id unarchiveObj = [NSKeyedUnarchiver unarchivedObjectOfClasses:[NSSet setWithObjects:[NSArray class], [GTListItem class],nil] fromData:readListData error:nil];
-    
-    
-    [[NSUserDefaults standardUserDefaults] setObject:listData forKey:@"listData"];
-    
-    NSData *testListData = [[NSUserDefaults standardUserDefaults]dataForKey:@"listData"];
-    id unarchiveObj = [NSKeyedUnarchiver unarchivedObjectOfClasses:[NSSet setWithObjects:[NSArray class], [GTListItem class],nil] fromData:readListData error:nil];
+
+//
+//    [[NSUserDefaults standardUserDefaults] setObject:listData forKey:@"listData"];
+//
+//    NSData *testListData = [[NSUserDefaults standardUserDefaults]dataForKey:@"listData"];
+//    id unarchiveObj = [NSKeyedUnarchiver unarchivedObjectOfClasses:[NSSet setWithObjects:[NSArray class], [GTListItem class],nil] fromData:readListData error:nil];
 
 
 	//查询文件
