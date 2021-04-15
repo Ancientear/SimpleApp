93d1ed71e15f32d773fa3cfb9dc042b711de0bc7
Ancientear <1136395374@qq.com>
2021-04-15 17:26:22 +0800

    使用AVPlayer播放视频

--- SimpleApp/Video/Views/GTVideoCoverView.m
+++ SimpleApp/Video/Views/GTVideoCoverView.m
@@ -6,6 +6,7 @@
 //
 
 #import "GTVideoCoverView.h"
+#import <AVFoundation/AVFoundation.h>
 
 @interface GTVideoCoverView()
 @property(nonatomic,strong,readwrite)UIImageView *coverView;
@@ -22,7 +23,7 @@
             _coverView = [[UIImageView alloc] initWithFrame:CGRectMake(0,0,frame.size.width,frame.size.height)];
             _coverView;
         })];
-        [self addSubview:({
+        [_coverView addSubview:({
             _playButton = [[UIImageView alloc] initWithFrame:CGRectMake((frame.size.width - 50)/2,(frame.size.height - 50)/2,50,50)];
             _playButton;
         })];
@@ -42,7 +43,30 @@
 
 
 #pragma mark - private public
+//视频播放的业务逻辑，首先是有一个占位图，占位图上有一个视频播放的按钮，当点击播放的时候，视频的界面粘贴到全部的界面上来
 - (void)_tapToPlay{
+    
+    NSURL *videoURL = [NSURL URLWithString:_videoUrl];
+    
+//    AVAsset *asset = [AVAsset assetWithURL:videoURL];
+//
+//    AVPlayerItem *videoItem = [AVPlayerItem playerItemWithAsset:asset];
+    
+    //AVPlayer *avPlayer = [AVPlayer playerWithPlayerItem:videoItem];
+    
+    //_videoUrl也可以直接生成avPlayer
+    AVPlayer *avPlayer = [AVPlayer playerWithURL:videoURL];
+    
+    //playLayer只提供画面的展示，不会响应手势
+    AVPlayerLayer *playLayer = [AVPlayerLayer playerLayerWithPlayer:avPlayer];
+    
+    //调整大小
+    playLayer.frame = _coverView.bounds;
+    [_coverView.layer addSublayer:playLayer];
+    
+    //播放器的播放
+    [avPlayer play];
     NSLog(@"");
+    
 }
 @end
