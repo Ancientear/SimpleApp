dbf6d107fedcc0c91e333ac6e420bf446ba758fd
Ancientear <1136395374@qq.com>
2021-04-16 18:34:22 +0800

    逻辑、物理分辨率适配

---
+++ SimpleApp/GTScreen.m
@@ -0,0 +1,28 @@
+//
+//  GTScreen.m
+//  SimpleApp
+//
+//  Created by 申凡 on 2021/4/16.
+//
+
+#import "GTScreen.h"
+
+@implementation GTScreen
+
+//iphone xs max
++ (CGSize)sizeFor65Inch{
+    return CGSizeMake(414,896);
+}
+
+//iphone xr
++ (CGSize)sizeFor61Inch{
+    return CGSizeMake(414,896);
+}
+
+// iphonex
++ (CGSize)sizeFor58Inch{
+    return CGSizeMake(375,812);
+}
+
+
+@end
