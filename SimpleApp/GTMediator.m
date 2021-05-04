be5c7954e2f71565394b008aaefaa2a44b6abc5a
Ancientear <1136395374@qq.com>
2021-05-04 16:20:22 +0800

    通过一个字符串来反射它的class

--- SimpleApp/GTMediator.m
+++ SimpleApp/GTMediator.m
@@ -8,12 +8,15 @@
 //这个类作为一个中转类
 //组件化--列表和新闻详情的解耦
 #import "GTMediator.h"
-#import "GTDetailViewController.h"
 
 
 @implementation GTMediator
+//依赖于GTDetailViewController，并没有彻底解耦,使用类的反射,通过一个字符串来反射它的class
 + (__kindof UIViewController *)detailViewControllerwithUrl:(NSString *)detailUrl{
-    GTDetailViewController *controller = [[GTDetailViewController alloc]initWithUrlString:detailUrl];
+    
+    Class detailCls = NSClassFromString(@"GTDetailViewController");
+
+    UIViewController *controller = [[detailCls alloc] performSelector:NSSelectorFromString(@"initWithUrlString:") withObject:detailUrl];
     return controller;
 }
 @end
